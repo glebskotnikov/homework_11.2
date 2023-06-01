5f25195102b7b2aebff3064edf9f34c489169b40
Gleb Skotnikov <skotnikov.nsk@gmail.com>
2023-06-01 14:29:22 +0600

    def input_string_2(value)

--- str_func.py
+++ str_func.py
@@ -1,3 +1,17 @@
 def input_string(value):
     return value.upper()
 
+
+def input_string_2(value):
+    """
+    Функция делает заглавными первые
+    буквы каждого слова в строке
+    """
+    words = value.split()
+    words_2 = []
+    for word in words:
+        words_2.append(word.capitalize())
+    return " ".join(words_2)
+
+
+
